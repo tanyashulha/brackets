63c780601bf3cb48b15d807dbf1ab708f31dec9c
Tatyana Shulha <tsulga6@gmail.com>
2019-09-23 08:38:46 +0300

    implement brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    var stack = [];
+    bracketsConfig = bracketsConfig.reduce((acc,v) => {
+        acc[v[0]] = v[1];
+        return acc;
+    },{});
+  
+    for (var i = 0; i < str.length; i++) {
+        for (var key in bracketsConfig) {
+          if (key === bracketsConfig[key] && stack.length && stack[stack.length - 1] === key) {
+              stack.pop();
+              continue;
+            }
+    
+            if ( str[i] === key ) {
+                stack.push(str[i]);
+                break;
+            }
+    
+            if (str[i] === bracketsConfig[key]) {
+                if (stack[stack.length-1] === key) {
+                    stack.pop();
+                    break;
+                } 
+                else {
+                    return false;
+                }
+            }
+        }
+      
+      }
+    return !stack.length;
+};
